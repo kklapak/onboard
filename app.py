96a72dd3c17a8b2e5a62d97a2fadb277c72ee101
Merlin <merlin@DESKTOP-0C7DR6I.localdomain>
2023-03-08 22:54:58 -0800

    Initial Commit

---
+++ app.py
@@ -0,0 +1,269 @@
+import os
+
+import requests, sys, logging
+import json
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+app.config['DEBUG'] = False
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
+app.config['SESSION_COOKIE_SAMESITE'] = None
+app.config['JIRA_URL'] = "https://jira.cfdata.org"
+DEBUG = app.config['DEBUG']
+
+products = ["Access", "Analytics", "API Gateway", "Area 1", "Argo", "Bot Management", "Browser Isolation",
+            "CASB", "CDN", "China Network", "Cloudflare Stream", "Data Localization", "DDoS Protection",
+            "DNS", "Email Routing", "Gateway", "Identity", "Images", "Load Balancing", "Logs", "Magic Firewall",
+            "Magic Transit",
+            "Magic WAN", "Network Interconnect (CNI)", "Page Shield", "Pages", "Premium Success", "R2", "Rate Limiting",
+            "Spectrum",
+            "SSL / TLS Encryption", "SSL / TLS for SaaS Providers", "Tunnel", "WAF", "Waiting Room", "WARP",
+            "Website Optimization Services",
+            "Workers", "Workers KV", "Zaraz", "Zero Trust"]
+
+def convert_custom_fields(fields):
+    custom_fields = {'sales_force_id':'customfield_20323',
+                'op_id':'customfield_20324',
+                 'start_date':'customfield_11300',
+               'region': 'customfield_18408',
+               'description':'description',
+               'customer':'summary'}
+    new_fields = {}
+    for field in fields.keys():
+        if custom_fields[field] not in ('customfield_18408'):
+            new_fields[custom_fields[field]] = str(fields[field])
+        else:
+            new_fields[custom_fields[field]] = {'value:': fields[field]}
+    return new_fields
+
+
+def make_request(cf_auth, token, method, url, payload=None, query=False):
+    COOKIES = {'SameSite': 'None', 'CF_Authorization': cf_auth}
+    HEADERS = {'Authorization': 'Bearer ' + token}
+
+    if not DEBUG or query:
+        try:
+            response = requests.request(method, url, cookies=COOKIES, headers=HEADERS, json=payload)
+            return response
+        except requests.exceptions.RequestException as e:
+            print(f'Request failed: {e}')
+            print(f'Request payload: {payload}')
+            return jsonify(e)
+    else:
+        print("Printing Payload instead of making REST request")
+        data = {
+            'method': method,
+            'url': url,
+            'payload': payload,
+            'query': query
+        }
+        return jsonify(data)
+
+def create_issue(cf_auth, token,payload):
+    url = app.config['JIRA_URL'] + '/rest/api/2/issue/'
+    response = make_request(cf_auth, token, 'POST', url, payload, False)
+    return response.json()
+
+def link_issues(cf_auth, token,epic_key, payload):
+    url = app.config['JIRA_URL'] + f'/rest/agile/1.0/epic/{epic_key}/issue'
+    return make_request(cf_auth, token,'POST', url, payload, False)
+
+def get_issue(cf_auth, token,issue_key):
+    url = app.config['JIRA_URL'] + f'/rest/api/2/issue/{issue_key}'
+    return make_request(cf_auth, token,'GET', url, query=True)
+
+def get_epic(cf_auth, token,epic_key):
+    url = app.config['JIRA_URL'] + f'/rest/agile/1.0/epic/{epic_key}/issue'
+    return make_request(cf_auth, token,'GET', url, query=True)
+
+def update_epic(cf_auth, token,epic_key,fields):
+    # Define the Jira API endpoint and headers
+    url = app.config['JIRA_URL'] + f'/rest/api/2/issue/{epic_key}'
+    payload = {
+        "update": {}
+    }
+    for field in fields:
+        payload["update"][field] = [{"set":fields[field]}]
+
+    return make_request(cf_auth, token,'PUT', url, payload, False)
+
+def jira_query(cf_auth, token,jql):
+    url = app.config['JIRA_URL'] + '/rest/api/2/search'
+    payload = {
+        "jql": jql,
+        "startAt": 0,
+        "maxResults": 50
+    }
+    response = make_request(cf_auth, token,'POST', url, payload, query=True)
+    return response
+
+def create_child_issues(cf_auth, token,epic_id, customer, sales_force_id, op_id, start_date, region, selected_products):
+    issue_keys = []
+    for product in selected_products:
+        # Make Jira API call for each product
+        issue = create_issue(cf_auth, token, {
+            'fields': {
+                'project': {'key': "ONBOARD"},
+                'summary': customer + " - " + str(product),
+                'issuetype': {'name': "Project"},
+                'customfield_20323': str(sales_force_id),
+                'customfield_20324': str(op_id),
+                "customfield_20325": {"value": str(product)},
+                "customfield_11300": start_date,
+                'customfield_18408': {"value": region}
+            }
+        })
+        print(issue)
+        if issue["key"]:
+            issue_keys.append(issue["key"])
+    print("Children created:" + str(issue_keys))
+    response = link_issues(cf_auth, token, epic_id, payload={"issues": issue_keys})
+    if response.status_code == 204:
+        return jsonify({'status':'success','epic_id':epic_id,'projects':issue_keys})
+    else:
+        return jsonify({'status': 'failed', 'epic_id': epic_id, 'projects': issue_keys, 'message':'Not Linked'})
+
+@app.route('/get-onboards', methods=["POST"])
+def get_epics_for_project(project_key="Onboard"):
+    cf_auth = request.form.get('cf_auth', default=None)
+    token = request.form.get('token', default=None)
+    #print(cf_auth, token)
+    if(token is not None):
+        jql = f"project={project_key} and issuetype=Epic"
+        response = jira_query(cf_auth,token,jql)
+        if response.status_code == 200:
+            if("CF_Authorization=deleted;" not in response.headers.get('Set-Cookie')):
+                response_data = response.json()
+                issues = response_data.get("issues")
+                epics = [{"key": issue["key"], "summary": issue["fields"]["summary"]} for issue in issues]
+                return jsonify(epics)
+            else:
+                return {"error": "Update your CF_Authorization token in app.py"}
+    print(f"Error retrieving Epics for project {project_key}")
+    return {"error": "Update your CF_Authorization token in app.py"}
+
+@app.route('/lookup_jira', methods=['POST'])
+def lookup(cf_auth=None,token=None,jira_id=None):
+    selected_value = request.form.get('selected_value')
+    lookup = jira_id or selected_value
+
+    cf_auth = request.form.get('cf_auth', default=cf_auth)
+    token = request.form.get('token', default=token)
+
+    epic = get_issue(cf_auth,token,lookup).json()
+    linked = get_epic(cf_auth,token,lookup).json()
+    #print(linked)
+
+    products = []
+    start_date = None
+    if linked['issues']:
+        for issue in linked['issues']:
+            products.append(issue.get('fields', {}).get('customfield_20325', {}).get('value'))
+            start_date = issue.get('fields', {}).get('customfield_11300', '')
+
+    results = {
+        'epic_id': lookup,
+        'customer': epic['fields']['summary'],
+        'description': epic['fields']['description'],
+        'salesforce': epic['fields']['customfield_20323'],
+        'opportunity': epic['fields']['customfield_20324'],
+        'region': epic['fields']['customfield_18408'].get('value', ''),
+        'products': products,
+        'start_date': start_date,
+    }
+    #print(results)
+    return jsonify(results)
+
+@app.route('/', methods=['GET','POST'])
+def form_processing():
+    action = request.form.get('action')
+    if action == 'create':
+        print("HIT CREATE")
+        # Get form data
+        cf_auth = request.form['cf_auth']
+        token = request.form['token']
+        customer = request.form['customer']
+        sales_force_id = request.form['salesforce']
+        op_id = request.form['opportunity']
+        description = request.form['description']
+        start_date = request.form['start_date']
+        region = request.form['region']
+
+        # Make Jira API call to create EPIC issue
+        #print(cf_auth,token, customer, sales_force_id, op_id, description, start_date, region)
+
+        epic = create_issue(cf_auth, token, {
+            'fields': {
+                'project': {'key': "ONBOARD"},
+                'summary': customer,
+                'description': description,
+                'issuetype': {'name': "Epic"},
+                'customfield_20323': str(sales_force_id),
+                'customfield_20324': str(op_id),
+                'customfield_18408': {'value': region},
+                'customfield_10701': customer
+            }
+        })
+
+        if "errors" in epic:
+            return jsonify(epic)
+        else:
+            epic_id = None or epic["key"]
+            print(epic_id)
+
+            if not epic_id:
+                return jsonify(epic)
+            else:
+                selected_products = [key for key in request.form.keys() if 'on' in request.form.getlist(key)]
+               # print(selected_products)
+                response = create_child_issues(cf_auth,token,epic_id,customer,sales_force_id, op_id, start_date, region, selected_products)
+                #print(response)
+                print("END CREATE")
+                return response
+        return jsonify(epic)
+    elif action == 'update':
+        print("HIT UPDATE")
+
+        epic_id, cf_auth, token, customer, sales_force_id, op_id, description, start_date, region = (
+            request.form.get(field, default=None) for field in
+            ('epic_id','cf_auth', 'token', 'customer', 'salesforce', 'opportunity', 'description', 'start_date', 'region')
+        )
+
+        response = lookup(cf_auth,token,epic_id)
+        epic = json.loads(response.data)
+
+        updated_fields = {}
+        if customer is not None and customer != '':
+            updated_fields["customer"] = customer
+        if sales_force_id is not None and sales_force_id != '':
+            updated_fields["sales_force_id"] = sales_force_id
+        if op_id is not None and op_id != '':
+            updated_fields["op_id"] = op_id
+        if description is not None and description != '':
+            updated_fields["description"] = description
+        if start_date is not None and start_date != '':
+            updated_fields["start_date"] = start_date
+        if region is not None and region != '':
+            updated_fields["region"] = region
+
+        selected_products = [key for key in request.form.keys() if 'on' in request.form.getlist(key)]
+        #print("New Products " + str(selected_products))
+
+        #print("Updated Fields " + str(updated_fields))
+
+        if len(updated_fields) > 0:
+            response = update_epic(cf_auth,token,epic_id, convert_custom_fields(updated_fields))
+
+        if len(selected_products) > 0:
+            issue_keys = []
+            response = create_child_issues(cf_auth,token,epic_id, epic["customer"], epic["salesforce"], epic["opportunity"], epic["start_date"], epic["region"], selected_products, issue_keys)
+        print("END UPDATE")
+        return jsonify(response)
+    else:
+        return render_template('index.html', debug=DEBUG)
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ static/js/app.js
@@ -0,0 +1,277 @@
+$(document).ready(() => {
+  addProductCheckboxes();
+
+  const searchbar = $('#search-bar');
+  var customerInput = $("input[name='customer']");
+
+  searchbar.select2({ dropdownAutoWidth: true, width: 'auto'})
+  $('.select2-container').hide()
+
+  var token = localStorage.getItem('token');
+  var cf_auth = localStorage.getItem('cf_auth');
+
+  if (token) {
+    $('#token').val(token);
+  }
+  if (cf_auth) {
+    $('#cf_auth').val(cf_auth);
+  }
+
+  $('#token').on('keydown', function(){
+      $(".select2-container").hide();
+      $("#loading").show();
+      token = $('#token').val()
+     // console.log("Jira Token " + token)
+      get_onboard_jiras(token,cf_auth)
+      localStorage.setItem('token', $('#token').val())
+  });
+
+  $('#cf_auth').on('keydown', function(){
+      $(".select2-container").hide();
+      $("#loading").show();
+      cf_auth = $('#cf_auth').val()
+    //  console.log("cf_auth " + cf_auth)
+      get_onboard_jiras(token,cf_auth)
+      localStorage.setItem('cf_auth', $('#cf_auth').val())
+  });
+
+ // console.log("Jira Token " + token)
+//  console.log("cf_auth " + cf_auth)
+  get_onboard_jiras(token,cf_auth)
+  console.log("Ready")
+
+  const searchCall = () => {
+    const selectedValue = $("#search-bar").val();
+    if (selectedValue) {
+      if (selectedValue !== ''){
+          console.log('Sending POST request...');
+          $.ajax({
+            url: '/lookup_jira',
+            type: 'POST',
+            data: { selected_value: selectedValue, token: token, cf_auth: cf_auth },
+            dataType: 'json',
+            success: function(data) {
+              console.log(data)
+              updateForm(data)
+              console.log("Sent data")
+            }
+          });
+      }else{
+        resetForm();
+      }
+    } else {
+      resetForm();
+    }
+  };
+
+  const resetForm = () => {
+    $("#create_epic")[0].reset();
+    $('.form-check-input').prop('checked', false);
+    $('#create_epic').find('*').prop('disabled', false);
+    $("button[id='submit']").show();
+    $("button[id='update']").hide();
+    $("#create_epic").show();
+  };
+
+  searchbar.on('change', searchCall);
+  searchbar.load(searchCall);
+
+  searchbar.on('select2:select', (e) => {
+    const { id: value, text } = e.params.data;
+    searchbar.val(value);
+  });
+
+  $('button[type="submit"]').click(function() {
+        // Set the value of the clicked button in a hidden input field
+        $('#submit-action').val($(this).attr('name'));
+  });
+
+  $('#create_epic').on('submit', function(event) {
+        // Prevent the form from submitting
+        event.preventDefault();
+        console.log("Creating Epic")
+
+        var token = $('#token').val();
+        var cf_auth = $('#cf_auth').val();
+        console.log(token,cf_auth)
+
+        var submitName = $('#submit-action').val();
+        console.log(submitName)
+
+        // Get the form data
+        var formData = $(this).serializeArray();
+        formData.push({name:'cf_auth',value: cf_auth});
+        formData.push({name:'token',value: token});
+        formData.push({name:'action',value: submitName});
+
+        // Get the value of the linked field
+        console.log(formData)
+
+        var message = $('#message');
+
+        $(this).find(':input').prop('disabled', true);
+        console.log('Sending POST request...');
+          $.ajax({
+            url: '/',
+            type: 'POST',
+            data: formData,
+            dataType: 'json',
+            success: function(response) {
+              console.log(response)
+              $('#create_epic').find(':input').prop('disabled', false);
+              if (response["errors"] === 'undefined'){
+                  message.text('Issue created successfully').removeClass('error').addClass('success').show();
+              }
+              else if (response["status"] === 'success'){
+                  get_onboard_jiras(token,cf_auth);
+                  message.html('<p><strong>Epic ID:</strong><a href="https://jira.cfdata.org/browse/' + response.epic_id + '" target="_blank">'+ response.epic_id +'</a>');
+                  message.append('<p><strong>Projects:</strong> ' + response.projects.join(', ') + '</p>');
+                  message.append('<p><strong>Status:</strong> ' + response.status + '</p>');
+                  message.removeClass('error').addClass('success').show();
+                  $('#search-bar').val(response.epic_id)
+                  $('#search-bar').trigger('change');
+              } else{
+                message.text("Error: " + JSON.stringify(response["errors"]));
+              }
+            },
+            error: function(xhr, textStatus, errorThrown) {
+                $('#create_epic').find(':input').prop('disabled', false);
+                console.error('Error:', xhr.responseText);
+            }
+          });
+
+      });
+
+});
+
+function get_onboard_jiras(token,cf_auth){
+  console.log("Grabbing ONBOARD Epics")
+  $.ajax({
+    url: '/get-onboards',
+    type: 'POST',
+    data: { token: token, cf_auth: cf_auth },
+    async: false,
+    dataType: 'json',
+    success: (data) => {
+      $('#search-bar').empty();
+      $('#search-bar').append('<option value="">Create New</option>')
+      if (data["error"] === undefined) {
+          data.forEach(({ key, summary }) => {
+            const optionText = `(${key}) ${summary}`;
+            $('#search-bar').append($('<option>', { text: optionText, value: key }));
+          });
+          $("#loading").hide();
+          $(".select2-container").show();;
+      }
+      else{
+          $(".select2-container").hide();
+          $("button[id='submit']").hide();
+          $("button[id='update']").hide();
+          $("#loading").text(data.error)
+          $("#loading").show();
+      }
+    },
+  });
+}
+
+function updateForm(data) {
+  const epicExistsInput = $("input[name='epic_exists']");
+  const epicIdInput = $("input[name='epic_id']");
+  const submitButton = $("button[id='submit']");
+  const updateButton = $("button[id='update']");
+  const descriptionTextarea = $("textarea[name='description']");
+  const checkboxInputs = $('.form-check-input');
+  const regionSelect = $("#region");
+
+  for (const [key, value] of Object.entries(data)) {
+    switch (key) {
+       case "epic_id":
+        if (value != null) {
+          epicExistsInput.val("true");
+          epicIdInput.val(value);
+          submitButton.hide();
+          updateButton.show();
+        }
+        break;
+
+      case "description":
+        if (value != null) {
+          descriptionTextarea.val(value).prop('disabled', true);
+        } else {
+          descriptionTextarea.val("").prop('disabled', false);
+        }
+        break;
+
+      case "products":
+        checkboxInputs.each(function() {
+          const checkboxValue = $(this).attr('name');
+          const objectInArray = value.includes(checkboxValue);
+          $(this).prop('checked', objectInArray).prop('disabled', objectInArray);
+        });
+        break;
+
+      case "region":
+        regionSelect.val(value).prop('disabled', true);
+        break;
+
+      default:
+        const input = $(`input[name='${key}']`);
+        if (value == null) {
+          input.val("").prop('disabled', false);
+        } else {
+          input.val(value).prop('disabled', true);
+        }
+        break;
+    }
+    //console.log(`${key}: ${value}`);
+  }
+}
+
+function addProductCheckboxes(){
+
+ // Define an array of product names
+    const products = ["Access", "Analytics", "API Gateway", "Area 1", "Argo", "Bot Management","Browser Isolation",
+    "CASB","CDN","China Network","Cloudflare Stream", "Data Localization", "DDoS Protection",
+    "DNS","Email Routing","Gateway","Identity","Images", "Load Balancing", "Logs", "Magic Firewall", "Magic Transit",
+    "Magic WAN", "Network Interconnect (CNI)", "Page Shield", "Pages", "Premium Success","R2","Rate Limiting","Spectrum",
+    "SSL / TLS Encryption", "SSL / TLS for SaaS Providers", "Tunnel","WAF","Waiting Room","WARP","Website Optimization Services",
+    "Workers", "Workers KV", "Zaraz", "Zero Trust"];
+
+    // Get the product list container element
+    const productList = document.getElementById("product-list-1");
+    const productList2 = document.getElementById("product-list-2");
+
+    // Loop through the products array and create a checkbox for each product
+
+    column = 1
+    for (let i = 0; i < products.length; i++) {
+      // Create a new product div
+      const newProductDiv = document.createElement("div");
+      newProductDiv.classList.add("form-check");
+
+      // Create a new product checkbox
+      const newProductCheckbox = document.createElement("input");
+      newProductCheckbox.classList.add("form-check-input");
+      newProductCheckbox.setAttribute("type", "checkbox");
+      newProductCheckbox.setAttribute("name", products[i]);
+      newProductCheckbox.setAttribute("id", products[i]);
+
+      // Create a new product label
+      const newProductLabel = document.createElement("label");
+      newProductLabel.classList.add("form-check-label");
+      newProductLabel.setAttribute("for", products[i]);
+      newProductLabel.textContent = products[i];
+
+      // Add the new product checkbox and label to the new product div
+      newProductDiv.appendChild(newProductCheckbox);
+      newProductDiv.appendChild(newProductLabel);
+
+      // Add the new product div to the product list container element
+      if(i > Math.round(products.length/2)) {
+        productList2.appendChild(newProductDiv);
+      } else{
+         productList.appendChild(newProductDiv);
+      }
+
+    }
+}
